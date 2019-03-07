fa8629151ce76ac5e5673d082e8675ffb6d47041
Fran Madueño <47178991+franmadu6@users.noreply.github.com>
2019-03-07 13:54:36 +0100

    Update ejercicioxml.py

--- ejercicioxml.py
+++ ejercicioxml.py
@@ -2,6 +2,10 @@ def nombreanimales(doc):
     lista = doc.xpath('///nombre/text()')
     return lista
 
+def contaranimales(doc):
+    count = doc.xpath('///estado/text()')
+    return count
+
 from lxml import etree
 doc = etree.parse('animales.xml')
 
@@ -12,12 +16,17 @@ for prov in nombreanimales(doc):
     
 #Ejercicio 2.Contar los animales que esten en algun estado concreto.(Ej:en peligro).
 
+for prov in contaranimales(doc):
+    print("Hay",prov,"animles en esta situacion.")
 
 #Ejercicio 3.Buscar una palabra clave en "Datos Curiosos".
+
 
 
 #Ejercicio 4.Pedir el numero de crias maximo que puede tener una especie por camada y imprima todas las especies que hay con ese numero de crias o mas.
 
 
+
 #Ejercicio 5.Comparar una caracteristica entre dos animales pedidos por teclado.
+
 
