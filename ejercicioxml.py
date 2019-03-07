5fbe4bb57164c7daf90cf82e0281818694cdd56f
Fran Madueño <47178991+franmadu6@users.noreply.github.com>
2019-03-07 13:17:21 +0100

    Update ejercicioxml.py

--- ejercicioxml.py
+++ ejercicioxml.py
@@ -9,3 +9,15 @@ doc = etree.parse('animales.xml')
 
 for prov in nombreanimales(doc):
     print(prov)
+    
+#Ejercicio 2.Contar los animales que esten en algun estado concreto.(Ej:en peligro).
+
+
+#Ejercicio 3.Buscar una palabra clave en "Datos Curiosos".
+
+
+#Ejercicio 4.Pedir el numero de crias maximo que puede tener una especie por camada y imprima todas las especies que hay con ese numero de crias o mas.
+
+
+#Ejercicio 5.Comparar una caracteristica entre dos animales pedidos por teclado.
+
