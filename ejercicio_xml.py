2eb27790f1f92db34ff3d83d90fd9da88cff6b76
Fran Madueño <47178991+franmadu6@users.noreply.github.com>
2019-03-10 14:25:50 +0100

    probando while

--- ejercicio_xml.py
+++ ejercicio_xml.py
@@ -19,7 +19,7 @@ for prov in nombreanimales(doc):
     print(prov)
     
 #Ejercicio 2.Contar los animales que esten en algun estado concreto.(Ej:en peligro).
-print("\n 2.Contar los animales que esten en algun estado concreto")
+print("\n 2.Contar los animales que esten en algun estado concreto.")
 
 print('''
 Chuleta de estados:
@@ -31,24 +31,19 @@ estado=str(input("\nDime el estado que quieres contar: "))
 print("Hay",contaranimales(doc,estado),"animales en esta situación.")
 
 #Ejercicio 3.Buscar una palabra clave en "Datos Curiosos".
-
 print("\n 3.Buscar una palabra clave en 'Datos Curiosos'.")
 busqueda=str(input("Dime que palabra quieres buscar: "))
 
 while True:
-    if busqueda == False:
-        print("Esa palabra no se encuentra, prueba de nuevo.")
-    else:
+    if busqueda == doc.xpath('count(//animales/datocurioso/text())'):
         print(buscarpalabras(doc,busqueda))
-    
+    else:
+        print("Esa palabra no se encuentra, prueba de nuevo.")
     break
         
 
-
 #Ejercicio 4.Pedir el numero de crias maximo que puede tener una especie por camada y imprima todas las especies que hay con ese numero de crias o mas.
-
 print("\n 4.Pedir el numero de crias maximo que puede tener una especie por camada y imprima todas las especies que hay con ese numero de crias o mas.")
 
 #Ejercicio 5.Comparar una caracteristica entre dos animales pedidos por teclado.
-
 print("\n 5.Comparar una caracteristica entre dos animales pedidos por teclado.")
