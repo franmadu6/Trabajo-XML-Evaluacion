d4981405727ee5a31c61a3e2ce6d04e750980d55
Fran Madueño <47178991+franmadu6@users.noreply.github.com>
2019-03-10 13:24:38 +0100

    inicio ejercicio3

--- ejercicio_xml.py
+++ ejercicio_xml.py
@@ -6,15 +6,20 @@ def contaranimales(doc,estado):
     count = doc.xpath('count(//animales[estado/text() = "%s"])'%estado)
     return count
 
+def buscarpalabras(doc):
+    search = doc.xpath()
+    return search
+
 from lxml import etree
 doc = etree.parse('animales.xml')
 
 #Ejercicio 1. Función que devuelve una lista con los nombres de todos los animales
-
+print("\n 1. Función que devuelve una lista con los nombres de todos los animales")
 for prov in nombreanimales(doc):
     print(prov)
     
 #Ejercicio 2.Contar los animales que esten en algun estado concreto.(Ej:en peligro).
+print("\n 2.Contar los animales que esten en algun estado concreto")
 
 print('''
 Chuleta de estados:
@@ -27,12 +32,12 @@ print("Hay",contaranimales(doc,estado),"animales en esta situación.")
 
 #Ejercicio 3.Buscar una palabra clave en "Datos Curiosos".
 
-
+print("\n 3.Buscar una palabra clave en 'Datos Curiosos'.")
 
 #Ejercicio 4.Pedir el numero de crias maximo que puede tener una especie por camada y imprima todas las especies que hay con ese numero de crias o mas.
 
-
+print("\n 4.Pedir el numero de crias maximo que puede tener una especie por camada y imprima todas las especies que hay con ese numero de crias o mas.")
 
 #Ejercicio 5.Comparar una caracteristica entre dos animales pedidos por teclado.
 
-
+print("\n 5.Comparar una caracteristica entre dos animales pedidos por teclado.")
