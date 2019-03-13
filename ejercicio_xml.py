79513f889e79bc3a1b0c0b0c1fb211bd3d2ba010
Fran Madueño <47178991+franmadu6@users.noreply.github.com>
2019-03-13 09:56:41 +0100

    ejercicio3

--- ejercicio_xml.py
+++ ejercicio_xml.py
@@ -1,29 +1,22 @@
-def nombreanimales(doc):
-    lista = doc.xpath('//nombre/text()')
-    return lista
-
-def contaranimales(doc,estado):
-    count = doc.xpath('count(//animales[estado/text() = "%s"])'%estado)
-    return count
-
-def buscarpalabras(doc,busqueda):
-    search = doc.xpath("//animales/[datocurioso/text() = '%a'] " %busqueda)
-    return search
-
-def numcrias(doc):
-    crias = doc.xpath('count(//animales/[numcrias/text() = "%b""])'%cria)
-    return crias
-
 from lxml import etree
 doc = etree.parse('animales.xml')
 
 #Ejercicio 1. Función que devuelve una lista con los nombres de todos los animales
 print("\n 1. Función que devuelve una lista con los nombres de todos los animales")
+
+def nombreanimales(doc):
+    lista = doc.xpath('//nombre/text()')
+    return lista
+
 for prov in nombreanimales(doc):
     print(prov)
     
 #Ejercicio 2.Contar los animales que esten en algun estado concreto.(Ej:en peligro).
 print("\n 2.Contar los animales que esten en algun estado concreto.")
+
+def contaranimales(doc,estado):
+    count = doc.xpath('count(//animales[estado/text() = "%s"])'%estado)
+    return count
 
 print('''
 Chuleta de estados:
@@ -34,20 +27,23 @@ Chuleta de estados:
 estado=str(input("\nDime el estado que quieres contar: "))
 print("Hay",contaranimales(doc,estado),"animales en esta situación.")
 
-#Ejercicio 3.Buscar una palabra clave en "Datos Curiosos".
-print("\n 3.Buscar una palabra clave en 'Datos Curiosos'.")
-busqueda=str(input("Dime que palabra quieres buscar: "))
+#Ejercicio 3.Buscar 'Datos Curiosos' de un animal indicado.
+print("\n 3.Buscar 'Datos Curiosos' de un animal indicado.")
 
-while True:
-    if busqueda == doc.xpath('count(//animales/datocurioso/text())'):
-        print(buscarpalabras(doc,busqueda))
-    else:
-        print("Esa palabra no se encuentra, prueba de nuevo.")
-    break
-        
+def buscarcurioso(doc,animal):
+    curiosidad = doc.xpath('//animales[nombre/text() = "%s"]/datocurioso/text()'%animal)[0]
+    return curiosidad
+
+animal=str(input("\n Dime el nombre del animal que quieres conocer sus datos curiosos: "))
+print("\n",buscarcurioso(doc,animal))
+    
 
 #Ejercicio 4.Pedir el numero de crias maximo que puede tener una especie por camada y imprima todas las especies que hay con ese numero de crias o mas.
 print("\n 4.Pedir el numero de crias maximo que puede tener una especie por camada y imprima todas las especies que hay con ese numero de crias o mas.")
+
+def numcrias(doc):
+    crias = doc.xpath('count(//animales/[numcrias/text() = "%b"])'%cria)
+    return crias
 
 cria=int(input("\n ¿Cuantas crias puede tener el animal que estas buscando?   "))
 for listacrias in numcrias(doc):
@@ -56,6 +52,10 @@ for listacrias in numcrias(doc):
 #Ejercicio 5.Comparar una caracteristica entre dos animales pedidos por teclado.
 print("\n 5.Comparar una caracteristica entre dos animales pedidos por teclado.")
 
-animal1=str(input("Dime el nombre del primer aninmal: ")
-animal2=str(input("Dime el nombre del segudno aninmal: ")
+def compara(doc):
+    companimal = doc.xpath('//animales/[peso/text() = "%c"]'%peso)
+    return companimal
+
+animal1 =str(input("Dime el nombre del primer aninmal: "))
+animal2 =str(input("Dime el nombre del segudno aninmal: "))
             
